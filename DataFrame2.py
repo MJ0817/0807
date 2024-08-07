32904675c03b2d5baba8493af8f6329516c0f48f
MJ0817 <minjoonyy@naver.com>
2024-08-07 17:20:18 +0900

    Update DataFrame2.py

--- DataFrame2.py
+++ DataFrame2.py
@@ -31,4 +31,4 @@ import matplotlib.pyplot as plt
 col_names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 data = pd.read_csv('./data/pima-indians-diabetes.data.csv',
                    names=col_names)
-data.describe().to_csv('./results/describe.csv')
+data.describe().to_csv('./results/describe.csv') #results라는 폴더 생성후 .csv파일생성
